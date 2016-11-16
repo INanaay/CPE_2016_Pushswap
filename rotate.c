6d16a054a6d3269a6de1903d480064a9890a6bbb
Nathan <nathan.lebon@epitech.eu>
2016-11-16 18:24:58 +0100

    rotate working

---
+++ rotate.c
@@ -0,0 +1,37 @@
+/*
+** rotate.c for rotate in /home/lnanaay/projets/CPE_2016_Pushswap
+** 
+** Made by Nathan Lebon
+** Login   <lnanaay@epitech.net>
+** 
+** Started on  Wed Nov 16 18:11:21 2016 Nathan Lebon
+** Last update Wed Nov 16 18:23:55 2016 Nathan Lebon
+*/
+
+#include "my.h"
+#include <string.h>
+
+void	ra(t_dlist *la)
+{
+  int	temp;
+
+  temp = la->beg->val;
+  remove_elem(la, 1);
+  add_end(la, temp);
+}
+
+void	rb(t_dlist *lb)
+{
+  int	temp;
+
+  temp = lb->beg->val;
+  remove_elem(lb, 1);
+  add_end(la, temp);
+}
+
+void	rr(t_dlist *la, t_dlist *lb)
+{
+  ra(la);
+  rb(lb);
+  my_putstr("rr");
+}
